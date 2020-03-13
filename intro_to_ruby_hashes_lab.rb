822c56996b223db2fdabec2ace118176f06a89b6
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-13 15:38:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -56,6 +56,22 @@ end
 # complexity.
 =end
 def monopoly_with_fourth_tier
-
-
-end
+  railBash={
+  :railroads=>{
+    :pieces =>4,
+    :rent_in_dollars=>{
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+      },
+    :names=>{
+      #Those keys are pointing to empty hash
+      :reading_railroad=>{},
+      :pennsylvania_railroad=>{},
+      :b_and_o_railroad=>{},
+      :shortline_railroad=>{}
+        }
+    }
+  }
+  end
