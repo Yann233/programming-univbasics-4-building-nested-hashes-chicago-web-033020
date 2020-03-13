898f83b4cbddd0699e96d634bb526f2c0ac49764
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-13 15:33:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,6 +39,7 @@ def monopoly_with_third_tier
       :four_pieces_owned=>200
       },
     :names=>{
+      #Those keys are pointing to empty hash
       :reading_railroad=>{},
       :pennsylvania_railroad=>{},
       :b_and_o_railroa=>{}
