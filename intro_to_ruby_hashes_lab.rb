85d4182a14a1b1413f1b53d1b91ff169feea575a
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-13 15:37:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,11 +48,14 @@ def monopoly_with_third_tier
     }
   }
 end
-
+##########################################3
+=begin
+# When you start writing the implementation for this method, copy the Hash
+# you built in the previous method. Run the tests. They will fail, but
+# they'll guide you in how to modify what you just did to have more
+# complexity.
+=end
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+
 
 end
